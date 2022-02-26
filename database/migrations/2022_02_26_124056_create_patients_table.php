b97112f91a20382ccdf0e0e91a700e9cb35820f8
kbtnoel <nomens2010@gmail.com>
2022-02-26 13:54:47 +0100

    Create 2022_02_26_124056_create_patients_table.php

---
+++ database/migrations/2022_02_26_124056_create_patients_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePatientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            
+            $table->id('patient_id')->unique();
+            $table->string('fname');           
+            $table->string('lname');
+            $table->datetime('dob');
+            $table->text('email');
+            $table->text('address');
+            $table->text('phone');
+            $table->text('gender');
+            $table->text('description');
+            $table->text('doctor');
+            $table->string('image');
+            $table->timestamps();
+        });
+    
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patients');
+    }
+}
