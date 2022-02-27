b1f9493dd2c61cef5f8e4ed19a101c14e9a68d20
kbtnoel <nomens2010@gmail.com>
2022-02-27 11:23:56 +0100

    Update PatientController.php

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -14,14 +14,14 @@ class PatientController extends Controller
     {
         return "I am a Patient";
         $patient_id = Auth::user()->patient_id;
-        $patient = Patient::where('patient_id', $patient_id)->first();
+        $patient_id = Patient::where('patient_id', $patient_id)->first();
         return view('Patient.patient-form');
     }
 
 
     public function showAllPatients(Request $request) {
         $patients = Patient::all();
-        $patient_id = $request->input('patient_id');        
+        $patients = $request->input('patient_id');        
         return view('patients',['patients' => $patients])
                ->with('patients.list');
 
